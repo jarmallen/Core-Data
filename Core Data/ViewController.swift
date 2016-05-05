200d387650bd7459d663cc66e49d0a0e70c13143
Jared Allen <jarmallen@gmail.com>
2016-05-05 10:43:28 -0700

    Commit again

--- Core Data/ViewController.swift
+++ Core Data/ViewController.swift
@@ -7,12 +7,91 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set core data
+        
+        let appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let context: NSManagedObjectContext = appDel.managedObjectContext
+       
+        /*
+        var newUser = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context)
+        
+        newUser.setValue("Aurora", forKey: "username")
+        
+        newUser.setValue("meow", forKey: "password")
+        
+        do {
+            
+            try context.save()
+        
+        } catch {
+            
+            // Handle error
+        
+        }
+ 
+        */
+        
+       
+        // Retrieve core data
+        
+        let request = NSFetchRequest(entityName: "Users")
+        
+        // request.predicate = NSPredicate(format: "username = %@", "Jared")
+        
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            
+            let results = try context.executeFetchRequest(request)
+            
+            if results.count > 0 {
+                
+                for result in results as! [NSManagedObject] {
+                    
+                    // context.deleteObject(result)
+                    
+                   // result.setValue("Wifey", forKey: "username")
+                    
+                   /* do {
+                        
+                        try context.save()
+                    
+                    } catch {
+                        
+                        // Error
+                        
+                    } */
+                    
+                    if let username = result.valueForKey("username") as? String {
+                        
+                        print(username)
+                        
+                    }
+                    
+                 
+                    
+                }
+            }
+        
+        } catch {
+            
+            // Handle error
+            
+        }
+        
+        
+
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
